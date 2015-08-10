f731bb0ef287cef7d2a356b96c0934ad2f71422d
Adam Doyle <adam.doyle@mercy.net>
2015-08-10 09:51:19 -0500

    modified:   src/main/java/seasonality/Seasonality.java 	new file:   src/main/resources/HospitalData2.txt 	new file:   src/main/resources/hospitalData.R 	deleted:    src/main/resources/log4j.properties 	modified:   src/main/resources/log4j.xml 	modified:   src/test/resources/dates.txt

--- src/main/java/seasonality/Seasonality.java
+++ src/main/java/seasonality/Seasonality.java
@@ -12,6 +12,7 @@ import java.util.Properties;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.time.DateUtils;
+import org.apache.log4j.Logger;
 
 import cascading.flow.AssemblyPlanner.Context;
 import cascading.flow.Flow;
@@ -39,10 +40,12 @@ import cascading.tuple.TupleEntry;
 
 public class Seasonality {
 	/** @param args */
+	private static final Logger LOG = Logger.getLogger(Seasonality.class);
+
+	public static final String DATE_FORMAT = "MM/dd/yyyy";
+
 	public static void main(String[] args) throws RuntimeException {
-		//String inputPath = args[0];
 		String inputPath = "./src/test/resources/dates.txt";
-		//String classifyPath = args[1];
 		String classifyPath = "./src/test/resources/results.txt";
 
 		// set up the config properties
@@ -61,7 +64,7 @@ public class Seasonality {
 		List<Date> holidays = new ArrayList<Date>();
 		List<Date> plusOne = new ArrayList<Date>();
 		List<Date> minusOne = new ArrayList<Date>();
-		SimpleDateFormat sdf = new SimpleDateFormat("mm/dd/YYYY");
+		SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT);
 		for (String dateString : holidayStrings) {
 			Date date = null;
 			try {
@@ -75,7 +78,7 @@ public class Seasonality {
 			minusOne.add(DateUtils.addDays(date, -1));
 			plusOne.add(DateUtils.addDays(date, 1));
 		}
-		
+
 		Fields date = new Fields("date");
 		Fields pmmlFields = new Fields("date", "Total", "Weekday", "Month",
 				"Year", "Holiday", "PlusOne", "MinusOne", "MONDAY", "TUESDAY",
@@ -84,51 +87,40 @@ public class Seasonality {
 				"AUGUST", "SEPTEMBER", "OCTOBER", "NOVEMBER", "DECEMBER");
 
 		// create source and sink taps
-		Tap inputTap = new FileTap(new TextDelimited(date),  inputPath);
+		Tap inputTap = new FileTap(new TextDelimited(date), inputPath);
 		Pipe pipe = new Pipe("plainCopy");
-		Each enrich = new Each(pipe, date,  new EnrichFunction(
-				//date,
-				pmmlFields,
-				holidays, plusOne, minusOne));//, pmmlFields);
+		Each enrich = new Each(pipe, date, new EnrichFunction(
+		// date,
+				pmmlFields, holidays, plusOne, minusOne));// , pmmlFields);
 		Tap classifyTap = new FileTap(new TextDelimited(true, "\t"),
 				classifyPath);
 
-		// handle command line options
-		// OptionParser optParser = new OptionParser();
-		// optParser.accepts( "pmml" ).withRequiredArg();
-
-		// OptionSet options = optParser.parse( args );
-
 		// connect the taps, pipes, etc., into a flow
 		FlowDef flowDef = FlowDef.flowDef().setName("classify")
-				.addSource(pipe, inputTap)
-				.addTail(enrich)
+				.addSource(pipe, inputTap).addTail(enrich)
 				.addSink("classify", classifyTap);
 
-		// build a Cascading assembly from the PMML description
-		// if( options.hasArgument( "pmml" ) )
-		{
-			// String pmmlPath = (String) options.valuesOf( "pmml" ).get( 0 );
-			String pmmlPath = "./src/main/resources/patient-seasonality.xml";
+		String pmmlPath = "./src/main/resources/patient-seasonality.xml";
 
-			PMMLPlanner pmmlPlanner = new PMMLPlanner()
-					.setPMMLInput(new File(pmmlPath))
-					.retainOnlyActiveIncomingFields()
-					.setDefaultPredictedField(
-							new Fields("predict", Double.class)); // default
-																	// value if
-																	// missing
-																	// from the
-																	// model
+		PMMLPlanner pmmlPlanner = new PMMLPlanner()
+				.setPMMLInput(new File(pmmlPath))
+				.retainOnlyActiveIncomingFields()
+				.setDefaultPredictedField(new Fields("predict", Double.class)); // default
+																				// value
+																				// if
+																				// missing
+																				// from
+																				// the
+																				// model
 
-			flowDef.addAssemblyPlanner(pmmlPlanner);
-			// }
+		flowDef.addAssemblyPlanner(pmmlPlanner);
+		// }
 
-			// write a DOT file and run the flow
-			Flow classifyFlow = flowConnector.connect(flowDef);
-			classifyFlow.writeDOT("dot/classify.dot");
-			classifyFlow.complete();
-		}
+		// write a DOT file and run the flow
+		Flow classifyFlow = flowConnector.connect(flowDef);
+		classifyFlow.writeDOT("dot/classify.dot");
+		classifyFlow.complete();
+
 	}
 
 	static class EnrichFunction extends BaseOperation<Context> implements
@@ -152,9 +144,10 @@ public class Seasonality {
 		public void operate(FlowProcess flow, FunctionCall<Context> call) {
 			TupleEntry argument = call.getArguments();
 			String dateString = argument.getString(0);
+			LOG.debug("DEBUG - Date " + dateString);
 			Date date = null;
 			try {
-				date = new SimpleDateFormat("mm/dd/YYYY").parse(dateString);
+				date = new SimpleDateFormat(DATE_FORMAT).parse(dateString);
 			} catch (ParseException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -162,46 +155,52 @@ public class Seasonality {
 			}
 			Calendar calendar = Calendar.getInstance();
 			calendar.setTime(date);
+			LOG.debug("DEBUG - Date " + date);
 
 			int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
 			int month = calendar.get(Calendar.MONTH);
 			int year = calendar.get(Calendar.YEAR);
 
-			Tuple result = new Tuple();
+			List<Object> list = new ArrayList<Object>(27);
 
-			result.add(date);
+			list.add(date);
 			// Total
-			result.add(-1);
-			result.add(dayOfWeek);
-			result.add(month);
-			result.add(year);
+			list.add(-1);
+			list.add(dayOfWeek);
+			list.add(month);
+			list.add(year);
 			// Holiday
-			result.add(dateInList(date, holidays));
+			list.add(dateInList(date, holidays));
 			// PlusOne
-			result.add(dateInList(date, plusOne));
+			list.add(dateInList(date, plusOne));
 			// MinusOne
-			result.add(dateInList(date, minusOne));
+			list.add(dateInList(date, minusOne));
 			// Day of Week
-			result.add(dayOfWeek == Calendar.MONDAY ? "TRUE" : "FALSE");
-			result.add(dayOfWeek == Calendar.TUESDAY ? "TRUE" : "FALSE");
-			result.add(dayOfWeek == Calendar.WEDNESDAY ? "TRUE" : "FALSE");
-			result.add(dayOfWeek == Calendar.THURSDAY ? "TRUE" : "FALSE");
-			result.add(dayOfWeek == Calendar.FRIDAY ? "TRUE" : "FALSE");
-			result.add(dayOfWeek == Calendar.SATURDAY ? "TRUE" : "FALSE");
-			result.add(dayOfWeek == Calendar.SUNDAY ? "TRUE" : "FALSE");
+			list.add(dayOfWeek == Calendar.MONDAY ? "TRUE" : "FALSE");
+			list.add(dayOfWeek == Calendar.TUESDAY ? "TRUE" : "FALSE");
+			list.add(dayOfWeek == Calendar.WEDNESDAY ? "TRUE" : "FALSE");
+			list.add(dayOfWeek == Calendar.THURSDAY ? "TRUE" : "FALSE");
+			list.add(dayOfWeek == Calendar.FRIDAY ? "TRUE" : "FALSE");
+			list.add(dayOfWeek == Calendar.SATURDAY ? "TRUE" : "FALSE");
+			list.add(dayOfWeek == Calendar.SUNDAY ? "TRUE" : "FALSE");
 			// Month of Year
-			result.add(month == Calendar.JANUARY ? "TRUE" : "FALSE");
-			result.add(month == Calendar.FEBRUARY ? "TRUE" : "FALSE");
-			result.add(month == Calendar.MARCH ? "TRUE" : "FALSE");
-			result.add(month == Calendar.APRIL ? "TRUE" : "FALSE");
-			result.add(month == Calendar.MAY ? "TRUE" : "FALSE");
-			result.add(month == Calendar.JUNE ? "TRUE" : "FALSE");
-			result.add(month == Calendar.JULY ? "TRUE" : "FALSE");
-			result.add(month == Calendar.AUGUST ? "TRUE" : "FALSE");
-			result.add(month == Calendar.SEPTEMBER ? "TRUE" : "FALSE");
-			result.add(month == Calendar.OCTOBER ? "TRUE" : "FALSE");
-			result.add(month == Calendar.NOVEMBER ? "TRUE" : "FALSE");
-			result.add(month == Calendar.DECEMBER ? "TRUE" : "FALSE");
+			list.add(month == Calendar.JANUARY ? "TRUE" : "FALSE");
+			list.add(month == Calendar.FEBRUARY ? "TRUE" : "FALSE");
+			list.add(month == Calendar.MARCH ? "TRUE" : "FALSE");
+			list.add(month == Calendar.APRIL ? "TRUE" : "FALSE");
+			list.add(month == Calendar.MAY ? "TRUE" : "FALSE");
+			list.add(month == Calendar.JUNE ? "TRUE" : "FALSE");
+			list.add(month == Calendar.JULY ? "TRUE" : "FALSE");
+			list.add(month == Calendar.AUGUST ? "TRUE" : "FALSE");
+			list.add(month == Calendar.SEPTEMBER ? "TRUE" : "FALSE");
+			list.add(month == Calendar.OCTOBER ? "TRUE" : "FALSE");
+			list.add(month == Calendar.NOVEMBER ? "TRUE" : "FALSE");
+			list.add(month == Calendar.DECEMBER ? "TRUE" : "FALSE");
+
+			Tuple result = new Tuple();
+			result.addAll(list.toArray());
+
+			LOG.debug("Output tuple " + result);
 
 			call.getOutputCollector().add(result);
 
@@ -213,7 +212,7 @@ public class Seasonality {
 					return 1d;
 				}
 			}
-			return 0d;					
+			return 0d;
 		}
 
 	}
